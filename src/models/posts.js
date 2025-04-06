3099eebd6603cc40173a130b31a8ff1767a0a489
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-04-06 23:36:01 +0300

    Update posts.js

--- src/models/posts.js
+++ src/models/posts.js
@@ -3,6 +3,7 @@ const postSchema = mongoose.schema({
     title:{ type: string, required: true },
     content:{ type: string, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true},
+    category: { type: string, required: true }
 });
 
 const posts = mongoose.model('posts', postSchema);
