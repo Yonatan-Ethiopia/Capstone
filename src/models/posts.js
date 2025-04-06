9a6d3ff633da4c159326383767ebf445fc0fefd2
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-04-06 23:34:59 +0300

    Update posts.js

--- src/models/posts.js
+++ src/models/posts.js
@@ -3,7 +3,6 @@ const postSchema = mongoose.schema({
     title:{ type: string, required: true },
     content:{ type: string, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true},
-    id: { type: string, required: true, unique: true}
 });
 
 const posts = mongoose.model('posts', postSchema);
